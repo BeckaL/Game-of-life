a04f732869036e227ad86efb74af572860ec3925
Becka <rebecca.lelew@gmail.com>
2018-12-05 14:22:48 +0000

    Adds some more tests for dead neighbours

--- lib/grid.rb
+++ lib/grid.rb
@@ -9,18 +9,16 @@ class Grid
     dead_cells = []
     @live_cells.each do |cell|
       movements.each do |movement|
-        dead_cells.push(movement.zip(cell).map { |x,y| x +y })
+        dead_cells << movement.zip(cell).map { |x, y| x + y }
       end
     end
-    dead_cells.uniq!.sort!
-    dead_cells.select { |cell| !@live_cells.include?(cell) }
+    dead_cells.uniq.select { |cell| !@live_cells.include?(cell) }.sort
   end
 
   private
   def movements
-    [[-1,-1], [-1, 0], [-1, 1],
-    [0, -1], [0, 1],
-    [1, -1], [1, 0], [1, 1]]
+    [[-1,-1], [-1, 0], [-1, 1], [0, -1],
+    [0, 1], [1, -1], [1, 0], [1, 1]]
   end
 
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -8,9 +8,23 @@ describe 'grid' do
 
   it 'generates array of dead neighbour cells, given live cells' do
     grid = Grid.new([[1,1], [2,2]])
-    expect(grid.dead_neighbours.length).to eq 12
 
     expect(grid.dead_neighbours).to eq [[0,0], [0,1], [0,2], [1,0], [1,2], [1,3],
                                         [2,0], [2,1], [2,3], [3,1], [3,2], [3,3]]
+  end
+
+  it 'generates array of dead neighbour cells, given live cells' do
+    grid = Grid.new([[2,1], [4,4], [-1,-1]])
+    expect(grid.dead_neighbours).to eq [[-2,-2], [-2, -1], [-2,0], [-1, -2], [-1, 0], [0, -2], [0, -1], [0,0], [1, 0], [1,1], [1,2], [2,0], [2,2], [3, 0], [3,1], [3,2], [3,3], [3, 4], [3,5], [4,3], [4,5], [5,3], [5,4], [5,5]]
+  end
+
+  it 'generates array of dead neighbour cells for clustered cells' do
+    clustered_grid = Grid.new([[-1, -1], [-1, 0], [0, -1], [1, 0]])
+    expect(clustered_grid.dead_neighbours).to eq [[-2,-2], [-2, -1], [-2, 0], [-2, 1], [-1, -2], [-1, 1], [0, -2], [0, 0], [0,1], [1, -2], [1,-1], [1, 1], [2, -1],[2, 0], [2, 1]]
+  end
+
+  it 'has no dead neighbours if given an empty grid' do
+    empty_grid = Grid.new([])
+    expect(empty_grid.dead_neighbours).to eq []
   end
 end
