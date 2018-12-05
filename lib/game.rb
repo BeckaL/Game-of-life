cc8365d82c25fa7affb172317bbfcd8698451bd8
Becka <rebecca.lelew@gmail.com>
2018-12-05 22:27:45 +0000

    Starts refactor of survival and generation rules

--- lib/game.rb
+++ lib/game.rb
@@ -13,8 +13,8 @@ class Game
   def surviving_live_cells
     return @live_cells.select do |living_cell|
       cell_neighbours = find_neighbours(living_cell)
-      alive_nbours = (cell_neighbours - @dead_cells).length
-      living_cell if (MIN_SURV_THRESH...MAX_SURV_THRESH).include?(alive_nbours)
+      alive_nbours = (cell_neighbours & @live_cells).length
+      live_cell_survival_rule(living_cell, alive_nbours)
     end
   end
 
@@ -22,17 +22,23 @@ class Game
     return @dead_cells.select do |dead_cell|
       cell_neighbours = find_neighbours(dead_cell)
       alive_neighbours = (cell_neighbours & @live_cells).length
-      dead_cell if GENERATION_THRESH == alive_neighbours
+      dead_cell_generation_rule(dead_cell, alive_neighbours)
     end
   end
 
   private
   def find_neighbours(cell)
     neighbours = []
-    movements.each do |movement|
-      neighbours << movement.zip(cell).map { |x, y| x + y }
-    end
+    movements.each { |move| neighbours << move.zip(cell).map { |x, y| x + y } }
     neighbours
+  end
+
+  def dead_cell_generation_rule(cell, alive_neighbours)
+    cell if GENERATION_THRESH == alive_neighbours
+  end
+
+  def live_cell_survival_rule(cell, alive_nbours)
+    cell if (MIN_SURV_THRESH...MAX_SURV_THRESH).include?(alive_nbours)
   end
 
   def movements
