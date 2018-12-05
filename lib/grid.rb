27fe1702649a6cc7f48600dce0e23d5ebb021f68
Becka <rebecca.lelew@gmail.com>
2018-12-05 14:00:05 +0000

    Calculates dead neighbour cells from a grid of live cells

--- lib/grid.rb
+++ lib/grid.rb
@@ -5,4 +5,22 @@ class Grid
     @live_cells = live_cells
   end
 
+  def dead_neighbours
+    dead_cells = []
+    @live_cells.each do |cell|
+      movements.each do |movement|
+        dead_cells.push(movement.zip(cell).map { |x,y| x +y })
+      end
+    end
+    dead_cells.uniq!.sort!
+    dead_cells.select { |cell| !@live_cells.include?(cell) }
+  end
+
+  private
+  def movements
+    [[-1,-1], [-1, 0], [-1, 1],
+    [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1]]
+  end
+
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -5,4 +5,12 @@ describe 'grid' do
     grid = Grid.new([[1,1], [2,2], [3,3]])
     expect(grid.live_cells).to eq [[1,1], [2,2], [3,3]]
   end
+
+  it 'generates array of dead neighbour cells, given live cells' do
+    grid = Grid.new([[1,1], [2,2]])
+    expect(grid.dead_neighbours.length).to eq 12
+
+    expect(grid.dead_neighbours).to eq [[0,0], [0,1], [0,2], [1,0], [1,2], [1,3],
+                                        [2,0], [2,1], [2,3], [3,1], [3,2], [3,3]]
+  end
 end
